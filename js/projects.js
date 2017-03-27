9034909ee05f8f27c9e20a2c3475e7c49956122b
Saber Rasheed <edrees.rasheed@yahoo.com>
2017-03-27 12:29:08 +0300

    added work ex and css style to the pages

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,27 @@
 $('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+	e.preventDefault()
+	$(this).tab('show')
 })
 $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
 $('#myTabs a:first').tab('show') // Select first tab
 $('#myTabs a:last').tab('show') // Select last tab
 $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+function makeAjaxRequest(){
+
+	$.ajax({
+		url: 'https://api.github.com/users/Saber20/repos?sort=updated&per_page=5',
+		type: 'GET',
+	}).done(function(repos) {
+		console.log(repos);
+	});
+
+}
+
+
+
+$(document).ready(function(){
+
+});
+
+
