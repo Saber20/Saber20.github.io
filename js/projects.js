46a835e1edc11cf12376506aab51eabd4635616c
Saber Rasheed <edrees.rasheed@yahoo.com>
2017-03-29 09:54:10 +0300

    added AJAX to project page

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,3 @@
-$('#myTabs a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
-$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
-$('#myTabs a:first').tab('show') // Select first tab
-$('#myTabs a:last').tab('show') // Select last tab
-$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
 
 function makeAjaxRequest(){
 
@@ -13,16 +5,44 @@ function makeAjaxRequest(){
 		url: 'https://api.github.com/users/Saber20/repos?sort=updated&per_page=5',
 		type: 'GET',
 	}).done(function(repos) {
-		console.log(repos);
+		$('#current').html('')
+		// $('#current').html((repos[0].name))//repos[0].html_url,repos[0].created_at
+		repos.forEach(function(repo){
+			var d=new Date(repo.created_at)
+			$('#current').append(`<a href="${repo.html_url}"  target= "_blank">${repo.name}</a> Created at: ${formatDate(d)}<br>`)
+
+
+		})
 	});
 
 }
 
+function formatDate(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
 
 
 $(document).ready(function(){
-	
-	
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		if($(this).html()==='Current')
+		{
+			makeAjaxRequest();
+		}
+		$(this).tab('show')
+
+
+	})
+   $('#myTabs a:first').tab('show') // Select first tab
+   makeAjaxRequest();
+
 });
 
 
